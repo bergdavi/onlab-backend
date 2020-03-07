117d91f31e9a915abcac9676d19f20aad31eedb6
David Berghammer <berghammer.david@gmail.com>
2020-03-07 15:24:06 +0100

    Added websocket PoC [WIP]

---
+++ src/main/java/com/bergdavi/onlab/gameservice/configuration/WebSocketConfig.java
@@ -0,0 +1,35 @@
+package com.bergdavi.onlab.gameservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * WebSocketConfig
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app", "/topic");
+        config.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gameplay");
+        registry.addEndpoint("/gameplay").withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        // TODO Auto-generated method stub
+        WebSocketMessageBrokerConfigurer.super.configureClientInboundChannel(registration);
+    }
+}
---
+++ src/main/java/com/bergdavi/onlab/gameservice/configuration/WebSocketSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.bergdavi.onlab.gameservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+/**
+ * WebSocketSecurityConfig
+ */
+@Configuration
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        
+        messages
+            .simpDestMatchers("/app/gameplay/*")
+                .authenticated();
+            // .simpSubscribeDestMatchers("/topic/gameplay/*")
+            //     .authenticated();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
